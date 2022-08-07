b1abdae9f26e6a56e7c4c0816bba2fad7e57c294
harivonyR <harivonyratefiarison@gmail.com>
2022-08-07 21:53:57 +0300

    CHECK txt to bson mongodb

--- param.js
+++ param.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+const EJSON = require('mongodb-extjson');
+const pipellineText = '{ "$match": {"pur_name":"bundle" } }';
+const pipepellinParsed = EJSON.parse(pipellineText, { relaxed: true })
+
 let param = {
     db : "cbm",
     collection : "daily_purchase",
@@ -10,72 +14,8 @@ let param = {
 
 let pipelline = [
     // Stage 1
-    {
-        $match: {
-           "day" : {"$gte" : new Date("2022-07-21"), "$lte" :new Date("2022-07-24")},
-         "pur_name" : "bundle", 
-         "pur_group" : "Mixte",
-         //"pur_mode":{"$ne":"LMS"},
-         "pur_bndle_longname" : { "$in" : [
-            "Stay IN",
-            "Stay In Touch",
-            "Akama Full",
-            "Akama1 Renouvellement",
-            "Akama 1",
-            "Akama Go",
-            "Akama Go+",
-            "Akama 7",
-            "Akama7 Renouvellement",
-            "Akama Plus",
-            "Akama 3",
-            "Akama3 Renouvellement",
-            "Akama Up"    
-        ]}
-            
-        }
-    },
-
-    // Stage 2
-    {
-        $project: {
-           "_id" : 0,
-           "day" : { "$dateToString" : { format : "%d/%m/%Y" , date : "$day"}},
-           "party_id" : 1, 
-           "pur_bndle_longname" : 1,
-           "pur_group" : 1,
-           "pur_amnt" : { "$divide" : [ "$pur_amnt", 1.2 ]},
-           "pur_bndle" : 1,
-        }
-    },
-
-    // Stage 3
-    {
-        $group: {
-        "_id" : { 
-          "day" : "$day" ,
-          "party_id" : "$party_id" ,
-          "pur_bndle_longname" : "$pur_bndle_longname" ,
-          "pur_group" : "$pur_group" },
-          "pur_amnt" : { "$sum" : "$pur_amnt" },
-          "nb_achat" : { "$sum" : 1 },
-        }
-    },
-
-    // Stage 4
-    {
-        $group: {
-        "_id" : { 
-          "day" : "$_id.day" ,
-          "pur_bndle_longname" : "$_id.pur_bndle_longname" ,
-          "pur_group" : "$_id.pur_group" },
-          "pur_amnt" : { "$sum" : "$pur_amnt" },
-          "nb_client" : { "$sum" : 1 },
-          "nb_achat" : { "$sum" : "$nb_achat" },
-        }
-    },
-
+    pipepellinParsed
 ]
-
 
 date = Date("2022-07-21")
 //console.log(date.toISOString())
