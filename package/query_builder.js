b4f2f09dff17089015abeee250287ed0a6fa29cd
harivonyR <harivonyratefiarison@gmail.com>
2022-08-16 17:05:06 +0300

    check lookup pipelline ok

--- package/query_builder.js
+++ package/query_builder.js
@@ -2,12 +2,26 @@ const path = require('path')
 const { matchDatePipelline } = require('./match')
 const { parse_to_bson } = require('./bson_parser')
 
-async function buildQuery(date_begin, date_end, database, collection, pipelline, file_path, file_name) {
-    
-        const pippelineDate = matchDatePipelline(date_begin, date_end)
-        const pipellineProject = parse_to_bson(pipelline)
+async function prepare(pipelline_raw_text){
+    let pipelline_prepared = []
+    pipelline_raw_text = pipelline_raw_text.split('#')
 
-        const pipe = [pippelineDate, pipellineProject]
+    console.log("split length :"+pipelline_raw_text.length)
+
+    pipelline_raw_text.forEach(element => {
+        pipelline_prepared.push(parse_to_bson(element))
+    });
+    
+    return pipelline_prepared
+}
+
+async function buildQuery(date_begin, date_end, database, collection, pipelline, file_path, file_name){
+        const pippelineDate = matchDatePipelline(date_begin, date_end)
+        const pipelline_prepared = await prepare(pipelline)
+        
+
+        const pipe = [pippelineDate, ...pipelline_prepared]
+        
         /*
         let param = {
             db: database,
